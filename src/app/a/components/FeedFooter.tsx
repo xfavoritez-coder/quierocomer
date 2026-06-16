cb8514927b9eebf2516b0558b750374a657bceef
Jaime <tuemail@gmail.com>
2026-06-16 19:59:23 -0400

    FeedFooter: lámpara 30px

--- src/app/a/components/FeedFooter.tsx
+++ src/app/a/components/FeedFooter.tsx
@@ -16,8 +16,8 @@ export default function FeedFooter({ isDark }: { isDark?: boolean }) {
         <img
           src="/genio-lamp.png"
           alt=""
-          width={31}
-          height={31}
+          width={30}
+          height={30}
           style={{ filter: 'grayscale(1)', opacity: isDark ? 0.55 : 0.45, objectFit: 'contain' }}
         />
         <span style={{
