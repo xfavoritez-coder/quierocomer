944fbc75cffb71d10b9b85f52191b00d70493e07
Jaime <tuemail@gmail.com>
2026-09-16 17:53:03 -0500

    fix(build): force-dynamic en todas las páginas con queries DB para evitar prerender timeout

--- src/app/p/[id]/page.tsx
+++ src/app/p/[id]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { redirect } from 'next/navigation'
 import { prisma } from '@/lib/prisma'
 import { slugify } from '@/lib/slugify'
--- src/app/qr/vendedores/page.tsx
+++ src/app/qr/vendedores/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic'
+
 import { prisma } from "@/lib/prisma";
 import VendedoresPage from "@/components/qr/vendedores/VendedoresPage";
 
